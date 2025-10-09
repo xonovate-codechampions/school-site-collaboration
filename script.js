18340cd2bdc2c19a9c8363cb875c95ff95e9ff5d
Elijah Tawiah Dotse <dotseelijah0@gmail.com>
2025-10-10 01:45:33 +1200

    Add course enrollment functionality and new styles for Robotics and Web Development pages

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ const navLinks = document.querySelector(".nav-links");
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
+
+
+const openBtn = document.getElementById("openFormBtn");
+const modal = document.getElementById("enrollModal");
+const closeBtn = document.querySelector(".close");
+
+openBtn.onclick = () => {
+  modal.style.display = "flex";
+};
+
+closeBtn.onclick = () => {
+  modal.style.display = "none";
+};
+
+window.onclick = (event) => {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+};
