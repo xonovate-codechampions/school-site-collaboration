880bb5642cea7c5037064605b01fbd8b1f2cf5a9
Elijah Tawiah Dotse <dotseelijah0@gmail.com>
2025-10-12 04:56:14 +1200

    Not yet done

--- auth.js
+++ auth.js
@@ -1,32 +1,30 @@
-const input = document.querySelector(".password");
-const toggleBtn = document.querySelector(".toggle-btn");
+const inputs = document.querySelectorAll(".password");
+const toggleBtns = document.querySelectorAll(".toggle-btn");
 
-toggleBtn.addEventListener("click", () => {
-  if (input.type === "password") {
-    input.type = "text";
-    toggleBtn.innerHTML = `<i class="fa-solid fa-eye-slash"></i>`;
-  } else {
-    input.type = "password";
-    toggleBtn.innerHTML = `<i class="fa-solid fa-eye"></i>`;
-  }
-});
-
-const Input = document.querySelector(".Password");
-const ToggleBtn = document.querySelector(".Toggle-btn");
-
-ToggleBtn.addEventListener("click", () => {
-  if (Input.type === "password") {
-    Input.type = "text";
-    ToggleBtn.innerHTML = `<i class="fa-solid fa-eye-slash"></i>`;
-  } else {
-    Input.type = "password";
-    ToggleBtn.innerHTML = `<i class="fa-solid fa-eye"></i>`;
-  }
+toggleBtns.forEach((toggleBtn, index) => {
+  toggleBtn.addEventListener("click", (e) => {
+    const input = inputs[index];
+    if (input.type === "password") {
+      input.type = "text";
+      toggleBtn.innerHTML = `<i class="fa-solid fa-eye-slash"></i>`;
+    } else {
+      input.type = "password";
+      toggleBtn.innerHTML = `<i class="fa-solid fa-eye"></i>`;
+    }
+  });
 });
 
 const submitBtn = document.querySelector(".submitbtn");
 const course = document.getElementById("course");
 const valid1 = document.getElementById("valid1");
+
+course.onchange = () => {
+  if (course.value === "") {
+    valid1.style.display = "block";
+  } else {
+    valid1.style.display = "none";
+  }
+};
 
 submitBtn.onclick = (event) => {
   event.preventDefault();
@@ -37,25 +35,29 @@ submitBtn.onclick = (event) => {
   }
 };
 
-input.onkeyup = () => {
-  if (input.value.length <= 5) {
-    input.style.border = "1px solid red";
+const Input = document.getElementById("password");
+
+Input.onkeyup = () => {
+  if (Input.value.length <= 5) {
+    Input.style.border = "1px solid red";
     document.getElementById("valid2").style.display = "block";
     document.getElementById(
       "valid2"
     ).innerHTML = `<small>"Password must be more than 5 characters"</small>`;
   } else {
     document.getElementById("valid2").style.display = "none";
-    input.style.border = "1px solid green";
+    Input.style.border = "1px solid green";
   }
 };
 
-Input.onkeyup = () => {
-  if (Input.value === input.value) {
-    Input.style.border = "1px solid green";
+const password = getElementById("Password");
+
+password.onkeyup = () => {
+  if (password.value === Input.value) {
+    password.style.border = "1px solid green";
     document.getElementById("valid3").style.display = "none";
   } else {
-    Input.style.border = "1px solid red";
+    password.style.border = "1px solid red";
     document.getElementById("valid3").style.display = "block";
     document.getElementById(
       "valid3"
